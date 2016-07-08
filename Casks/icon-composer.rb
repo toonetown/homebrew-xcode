911b95bf820d60f3f79abc2326acd6c16ff4a431
Nathan Toone <nathan@toonetown.com>
2016-07-08 11:10:42 -0600

    Move the url specification into a separate function (for easier definition of casks/formulas)

--- Casks/icon-composer.rb
+++ Casks/icon-composer.rb
@@ -6,7 +6,7 @@ cask 'icon-composer' do
   version '2.5'
   sha256 'ac835dde82c605b6602ca0fd117b51402a8b307f746fab281f93d89b6c9b24c9'
 
-  url "#{XCODE_DOWNLOAD_PATH}/Graphics_Tools_for_Xcode_7.2.dmg"
+  url xcode_url(AC_DOWNLOAD_URL)
   name 'Icon Composer'
   homepage DEV_HOMEPAGE
   license :commercial
--- lib/xcode-paths.rb
+++ lib/xcode-paths.rb
@@ -2,6 +2,8 @@ XCODE_DOWNLOAD_PATH = (ENV['XCODE_DOWNLOAD_PATH'] ? ENV['XCODE_DOWNLOAD_PATH'] :
 AC_DOWNLOAD = 'http://adcdownload.apple.com/Developer_Tools'.freeze
 DEV_HOMEPAGE = 'https://developer.apple.com/downloads/'.freeze
 
+def xcode_url(ac_download_url); "#{XCODE_DOWNLOAD_PATH}/#{ac_download_url.split("/")[-1]}"; end
+
 def xcode_caveats(ac_download_url); <<-EOS.undent
   You must specify a location for the download files by setting the
   environment variable 'XCODE_DOWNLOAD_PATH'.  This can be specified as a
