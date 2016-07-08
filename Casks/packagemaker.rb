8ebc5f32482db03573c27e4b405582e8a05cabff
Nathan Toone <nathan@toonetown.com>
2016-07-08 11:13:43 -0600

    Add packagemaker (3.0.6)

---
+++ Casks/packagemaker.rb
@@ -0,0 +1,17 @@
+require 'pathname'
+require Pathname(path).realpath.dirname.join('../lib', 'xcode-paths')
+AC_DOWNLOAD_URL = 'auxiliary_tools_for_xcode__late_july_2012/xcode44auxtools6938114a.dmg'.freeze
+
+cask 'packagemaker' do
+  version '3.0.6'
+  sha256 '57eb1ec7f13cc36b56ee517f564fa2ad5cc2ab337b90aecbba5d1dbd08a7d203'
+
+  url xcode_url(AC_DOWNLOAD_URL)
+  name 'PackageMaker'
+  homepage DEV_HOMEPAGE
+  license :commercial
+
+  app 'PackageMaker.app', target: 'Developer/PackageMaker.app'
+
+  caveats xcode_caveats(AC_DOWNLOAD_URL)
+end
