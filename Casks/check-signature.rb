faa5603f347619b0dad5f1acbb7a751340439539
Nathan Toone <nathan@toonetown.com>
2016-08-23 10:00:54 -0600

    Add check-signature (1.0)

---
+++ Casks/check-signature.rb
@@ -0,0 +1,17 @@
+require 'pathname'
+require Pathname(path).realpath.dirname.join('../lib', 'xcode-common')
+AC_DOWNLOAD_URL = 'signaturecheck/signaturecheck.dmg'.freeze
+
+cask 'check-signature' do
+  version '1.0'
+  sha256 '4f269b302fc8106406fe539af133866425a798fcd89f573be08ccc68fdfdd161'
+
+  url xcode_url(AC_DOWNLOAD_URL)
+  name 'Signature Check'
+  homepage DEV_HOMEPAGE
+  license :commercial
+
+  binary 'check-signature'
+
+  caveats xcode_caveats(AC_DOWNLOAD_URL)
+end
