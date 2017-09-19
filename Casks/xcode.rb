9636db7ddaa17852770ba790eb9a27fa6b51d571
Nathan Toone <nathan@toonetown.com>
2017-09-19 17:07:32 -0600

    Update Xcode (9)

--- Casks/xcode.rb
+++ Casks/xcode.rb
@@ -1,10 +1,10 @@
 require 'pathname'
 require Pathname(@path).realpath.dirname.join('../lib', 'xcode-common') unless defined?(xcode_common)
-AC_DOWNLOAD_URL = 'Xcode_8.3.3/Xcode8.3.3.dmg'.freeze
+AC_DOWNLOAD_URL = 'Xcode_9/Xcode_9.dmg'.freeze
 
 cask 'xcode' do
-  version '8.3.3'
-  sha256 'b48ba6a9dbd20a2c246f5e37c28131be65334366642275a32fbd24797cde5360'
+  version '9'
+  sha256 '991d73df504ac13879e15611365d02e4f7b15b520fc0da99196d34fc5f10ebf9'
 
   url xcode_url(AC_DOWNLOAD_URL)
   name 'Xcode'
@@ -22,7 +22,7 @@ cask 'xcode' do
   app 'Xcode.app'
 
   postflight do
-    app_location = appdir.join(@cask.artifacts[:app].first.first).to_s
+    app_location = appdir.join(@cask.artifacts[:app].first.summarize).to_s
     
     # Select this version of xcode
     ohai 'Selecting default version of Xcode (may require sudo)'
@@ -41,5 +41,5 @@ cask 'xcode' do
     system '/usr/bin/sudo', '-E', '--', '/usr/bin/xcode-select', '--reset'
   end
 
-  caveats xcode_xip_caveats(AC_DOWNLOAD_URL, artifacts[:app].first.first)
+  caveats xcode_xip_caveats(AC_DOWNLOAD_URL, artifacts[:app].first.summarize)
 end
