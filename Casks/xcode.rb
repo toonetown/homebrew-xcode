b5cb7fc2294ac14344f15db7889f265dd5d39fe6
Nathan Toone <nathan@toonetown.com>
2018-04-25 16:34:13 -0600

    Update xcode (9.3)

--- Casks/xcode.rb
+++ Casks/xcode.rb
@@ -1,10 +1,10 @@
 require 'pathname'
 require Pathname(@path).realpath.dirname.join('../lib', 'xcode-common') unless defined?(xcode_common)
-AC_DOWNLOAD_URL = 'Xcode_9.2/Xcode_9.2.dmg'.freeze
+AC_DOWNLOAD_URL = 'Xcode_9.3/Xcode_9.3.dmg'.freeze
 
 cask 'xcode' do
-  version '9.2'
-  sha256 '9e6b7dc568a06faf0cc9cc82641e9dedb84e4306976457c485d4c8ab164e41e4'
+  version '9.3'
+  sha256 '1506ed99d2a82a36eb53c26b285cb6718ade1171f97e26fdfd4666a70894ca95'
 
   url xcode_url(AC_DOWNLOAD_URL)
   name 'Xcode'
