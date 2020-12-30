4f05e86c6130df843fdbb3629b7a1ce8b41f8bff
Nathan Toone <nathan@toonetown.com>
2020-12-30 09:09:09 -0700

    Update xcode 10.3 to patched version (for use on Big Sur)

--- Casks/xcode@10.3.rb
+++ Casks/xcode@10.3.rb
@@ -1,10 +1,10 @@
 require 'pathname'
 require Pathname(@path).realpath.dirname.join('../lib', 'xcode-common') unless defined?(xcode_common)
-AC_DOWNLOAD_URL = 'Xcode_10.3-slimmed.dmg'.freeze
+AC_DOWNLOAD_URL = 'Xcode_10.3-patched.dmg'.freeze
 
 cask 'xcode@10.3' do
   version '10.3'
-  sha256 'f178750e48cdf2f7c0ff5116b2483263056665bd749b2521d2ae430e79dbf662'
+  sha256 'd237dee929d6a88cb3758317a3bb8d3b4605cfcda01df3745201e02c313e337c'
 
   url xcode_url(AC_DOWNLOAD_URL)
   name 'Xcode'
